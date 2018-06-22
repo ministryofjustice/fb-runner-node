d6e76bda354dec19b50f2dc63c1d1843d641217e
Alex Robinson <github@alex.fu2k.org>
2018-06-22 05:48:06 +0100

    Load data before running middleware

--- .wallaby.conf.js
+++ .wallaby.conf.js
@@ -2,23 +2,21 @@ module.exports = () => ({
   files: [
     { pattern: 'lib/**/*.js', load: false },
     "!lib/**/*.unit.spec.js",
-    { pattern: 'app/**/*.js', load: false },
-    "!app/**/*.unit.spec.js",
     { pattern: 'bin/**/*.js', load: false },
     { pattern: 'data/**/*.json', load: false },
+    { pattern: 'lib/spec/**/*.json', load: false },
     { pattern: 'src/views/**/*', load: false },
     { pattern: 'dist/**/*', load: false }
   ],
   tests: [
-    'lib/**/*.unit.spec.js',
-    'app/**/*.unit.spec.js'
+    'lib/**/*.unit.spec.js'
   ],
   env: {
     type: 'node'
   },
   testFramework: 'tape',
   workers: {
-    recycle: true,
+    restart: true,
     initial: 1,
     regular: 1
   }
