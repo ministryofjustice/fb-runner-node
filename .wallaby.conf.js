c7e56c2e1e2e626c7fdd1591f29707d1a2e566b1
Alex Robinson <github@alex.fu2k.org>
2019-03-06 15:33:22 +0000

    Access env via method so wallaby does not need to ignore constants test

--- .wallaby.conf.js
+++ .wallaby.conf.js
@@ -8,9 +8,9 @@ module.exports = () => ({
     { pattern: 'src/views/**/*', load: false },
     { pattern: 'dist/**/*', load: false }
   ],
+  tests_notes: 'https://glebbahmutov.com/blog/how-to-correctly-unit-test-express-server/ - tried delete require.cache[require.resolve(\'serve-static\')]',
   tests: [
     'lib/**/*.unit.spec.js',
-    "!lib/constants/*.*.unit.spec.js",
     "!lib/middleware/routes-cached/*.unit.spec.js",
     "!lib/middleware/routes-static/*.unit.spec.js"
   ],
