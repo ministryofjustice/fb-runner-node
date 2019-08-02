454dcc6e3e53a2aae9ce41575b93bdb872edd0c9
Alex Robinson <github@alex.fu2k.org>
2019-08-02 21:49:55 +0100

    Catch unhandled exceptions in start script

--- bin/start.js
+++ bin/start.js
@@ -1,6 +1,14 @@
 const path = require('path')
 process.env.APP_DIR = path.resolve(__dirname, '..')
 
-const server = require('../lib/server/server')
-
-server.start()
+const start = async () => {
+  try {
+    const server = require('../lib/server/server')
+    await server.start()
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e)
+    process.exit(1)
+  }
+}
+start()
